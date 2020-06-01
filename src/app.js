c7455bda7edb518f16696020ba7df2e046108fd3
Khanh Dang <dangdovankhanh@gmail.com>
2020-06-01 13:07:54 +1000

    edit path

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const upload = require("./utils/upload");
 const helpers = require("./utils/helpers");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 //Define paths for Express config
 const publicDirectory = path.join(__dirname, "../public");
